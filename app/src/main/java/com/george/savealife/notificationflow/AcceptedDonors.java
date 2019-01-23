bb4627c56b09813b271c70b53cea06e7731c78ef
swaroop-nath <swaroopnath6@gmail.com>
2019-01-23 12:12:14 +0530

    Updates as of Jan 23, 2019 Features updated - Activity and Dialog created for notification click, along with linking them to notification click. Proper definition left. Changes desired in index.js - Add a phone number field to send the phone number field of the help seeker to display to the donors.

---
+++ app/src/main/java/com/george/savealife/notificationflow/AcceptedDonors.java
@@ -0,0 +1,20 @@
+package com.george.savealife.notificationflow;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.george.savealife.R;
+
+public class AcceptedDonors extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accepted_donors);
+        //TODO:
+        /*
+        1. Find a reference to the realtime database and collect data.
+        2. Fill an adapter and complete the list view.
+        3. Update the adapter when realtime database changes and notify the list view.
+         */
+    }
+}
